c928b553b8edd19dcf868be1ff40b5b33bd86cd0
tuongvi998 <tuongvi998@users.noreply.github.com>
2019-11-14 10:36:28 +0700

    interface

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -19,45 +19,53 @@ class Nav extends React.Component{
    
         return(
           <>
-              <nav className="navbar navbar-expand-lg navbar-light bg-color">
-                <div className="logo">
-                  <img className="img" src = {require('../TVT.PNG')} alt = "logo"></img>
-                </div>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                      <Link className="name" to="/"><i className="fas fa-home"></i> Trang Chủ<span className="sr-only">(current)</span></Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="name" to="/truyenhot"><i className="fab fa-hotjar"></i> Truyệt hot</Link>
-                    </li>
-                    <li className="nav-item subnav" >
-                      <Link className="name" to="/Category">Thể loại</Link><i class="fas fa-caret-down"></i>
-                      <div class="subnav-content">
-                        <option value="#F4F4F4">Xám nhạt</option>
-                        <option value="#DFDFE3">Xám Đậm</option>
-                        <option value="#E9EBEE">Xanh nhạt</option>
-                        <option value="#F4F4E4">Vàng nhạt</option>
-                        <option value="#EAE4D3">Màu sepia</option>
-                        <option value="#D5D8DC">Xanh đậm</option>
-                      </div>
-                    </li> 
-                    <li className="nav-item">
-                      <Link className="name" to="/truyenmoi" ><i className="fas fa-newspaper"></i> Truyện mới</Link>
-                    </li>
-                  </ul>
-                  <form className="form-inline my-2 my-lg-0 form">
-                    <input className="input" type="search" placeholder="Tìm truyện..." aria-label="Search" />
-                   <Link className="btn btn-primary" id="btnsearch" to="/Search"><i className="fas fa-search btnsearch"></i></Link>
-                  </form>
-                </div>
-                <div className ="sign">
-                  {this.login_logout()}                  
+          <div className="row">
+              <nav className="navbar navbar-expand-md navbar-expand-sm navbar-light bg-color">
+                <div className="row  ml-4">
+                  <div className="logo">
+                    <img className="img" src = {require('../TVT.PNG')} alt = "logo"></img>
+                  </div>
+                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                  </button>
+                  <div className="collapse navbar-collapse mr-3" id="navbarSupportedContent">
+                    <div className="row">
+                    <ul className="navbar-nav mx-auto mr-5">
+                      <li className="nav-link active">
+                        <Link className="name" to="/"><i className="fas fa-home"></i> Trang Chủ<span className="sr-only">(current)</span></Link>
+                      </li>
+                      <li className="nav-link">
+                        <Link className="name" to="/truyenhot"><i className="fab fa-hotjar"></i> Truyệt hot</Link>
+                      </li>
+                      <li className="nav-link subnav" >
+                        <Link className="name" to="/Category">Thể loại</Link><i class="fas fa-caret-down"></i>
+                        <div class="subnav-content">
+                          <option value="#F4F4F4">Xám nhạt</option>
+                          <option value="#DFDFE3">Xám Đậm</option>
+                          <option value="#E9EBEE">Xanh nhạt</option>
+                          <option value="#F4F4E4">Vàng nhạt</option>
+                          <option value="#EAE4D3">Màu sepia</option>
+                          <option value="#D5D8DC">Xanh đậm</option>
+                        </div>
+                      </li> 
+                      <li className="nav-link">
+                        <Link className="name" to="/truyenmoi" ><i className="fas fa-newspaper"></i> Truyện mới</Link>
+                      </li>
+                    </ul>
+                    <div className="row ml-5">
+                      <form className="form-inline my-2 my-lg-0 form">
+                        <input className="input" type="search" placeholder="Tìm truyện..." aria-label="Search" />
+                      <Link className="btn btn-primary" id="btnsearch" to="/Search"><i className="fas fa-search btnsearch"></i></Link>
+                      </form>
                     </div>
+                    <div className ="row ml-5 mt-2">
+                      {this.login_logout()}                  
+                    </div>
+                    </div>
+                  </div>
+                </div>
               </nav>
+            </div>
         </>
 
         )
