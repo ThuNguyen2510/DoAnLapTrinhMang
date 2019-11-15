53bd23ee245f960b29533f1bf16fc80002fcf49d
tuongvi998 <tuongvi998@users.noreply.github.com>
2019-11-15 23:57:28 +0700

    edit nav & index

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -16,20 +16,9 @@ class index extends React.Component{
                     <div className="col-md-2 col-lg-2" >
                         <Content/>
                     </div>
-                    <div className="col-md-10 col-lg-10 content">
+                    <div className="col-md-10 col-lg-10 ">
                         <Nav/>
                         <Hello/>
-                        <p>
-                        This Bootstrap sidebar layout by Brenna Veen would be a great fit for a modern web application. As you can see from the screenshot, this sidebar is positioned on the left side of the webpage. The white texts are under a black background. This sidebar contains navigation menus clicking which the user can navigate to the various pages of the website.
-
-Keep in mind that you don’t necessarily have to use a sidebar for only showing navigation menus, you can also display other things such as your contact information or your company information in the sidebar section. This sidebar has a vertical scroll bar on the right side, which allows the user to scroll down. On a smaller screen or a small browser window, the user can see the remaining menus in the sidebar using the scroll bar.
-This Bootstrap sidebar layout by Brenna Veen would be a great fit for a modern web application. As you can see from the screenshot, this sidebar is positioned on the left side of the webpage. The white texts are under a black background. This sidebar contains navigation menus clicking which the user can navigate to the various pages of the website.
-
-Keep in mind that you don’t necessarily have to use a sidebar for only showing navigation menus, you can also display other things such as your contact information or your company information in the sidebar section. This sidebar has a vertical scroll bar on the right side, which allows the user to scroll down. On a smaller screen or a small browser window, the user can see the remaining menus in the sidebar using the scroll bar.
-This Bootstrap sidebar layout by Brenna Veen would be a great fit for a modern web application. As you can see from the screenshot, this sidebar is positioned on the left side of the webpage. The white texts are under a black background. This sidebar contains navigation menus clicking which the user can navigate to the various pages of the website.
-
-Keep in mind that you don’t necessarily have to use a sidebar for only showing navigation menus, you can also display other things such as your contact information or your company information in the sidebar section. This sidebar has a vertical scroll bar on the right side, which allows the user to scroll down. On a smaller screen or a small browser window, the user can see the remaining menus in the sidebar using the scroll bar.
-                        </p>
                     </div>
                 </div>
             </div>
